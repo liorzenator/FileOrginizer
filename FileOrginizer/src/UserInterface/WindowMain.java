53f79891ed7429e53cdbf5ff1a1db9b21ea26562
Lior <Lior@DESKTOP-1GVO9GM.mynet>
2016-08-26 23:52:53 +0300

    added GUI

---
+++ FileOrginizer/src/UserInterface/WindowMain.java
@@ -0,0 +1,69 @@
+package UserInterface;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JSplitPane;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class WindowMain
+{
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable() {
+			public void run()
+			{
+				try
+				{
+					WindowMain window = new WindowMain();
+					window.frame.setVisible(true);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public WindowMain() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize()
+	{
+		frame = new JFrame();
+		frame.setBounds(100, 100, 644, 539);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnAddDirectory = new JButton("Add Directory");
+		btnAddDirectory.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				System.out.println("This is just a test");
+			}
+		});
+		btnAddDirectory.setBounds(10, 11, 124, 23);
+		frame.getContentPane().add(btnAddDirectory);
+	}
+}
